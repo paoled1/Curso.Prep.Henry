0f400f4f4ce32296a3ceb383746677ec68264453
Admin <paola.a.ledesma@gmail.com>
2021-11-17 19:46:55 -0300

    06-js-v  completo

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -46,8 +46,8 @@ function agregarStringInvertida() {
   String.prototype.reverse = function () {
     var stringInvertida ='';
     for (var i= this.length - 1 ; i >=0 ; i--){
-      stringInvertida = stringInvertida + this.charAt(i);
-      //stringInvertida +=this[i];
+      //stringInvertida = stringInvertida + this.charAt(i);
+      stringInvertida +=this[i];
     }
     return stringInvertida;
   }
